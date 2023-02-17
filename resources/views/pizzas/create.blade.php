7ae538aa57d1230cbccd8b25b639e88f89e094b5
Beata Gonera <beataiwonagonera@gmail.com>
2023-02-17 21:40:10 +0100

    routes protecting

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,24 +1,24 @@
-@extends('layouts.layout')
+@extends('layouts.app')
 
 @section('content')
 
-<form class="w-[80vw]" action='/pizzas' method='POST'>
+<form class="w-25 p-3" action='/pizzas' method='POST'>
     @csrf
-  <div class="flex flex-col gap-4 items-center border-b border-teal-500 py-2">
+  <div class="col">
 
     <label for='type' class="block text-gray-700 text-sm font-bold mb-2">Type of pizza:</label>
-    <input class="shadow appearance-none border rounded w-1/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" aria-label="pizza type" name='type'>
+    <input class="form-control" type="text" aria-label="pizza type" name='type'>
 
     <label for='base' class="block text-gray-700 text-sm font-bold mb-2">Base:</label>
-    <input class="shadow appearance-none border rounded w-1/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" aria-label="base" name='base'>
+    <input class="form-control" type="text" aria-label="base" name='base'>
 
     <label for='name' class="block text-gray-700 text-sm font-bold mb-2">Order for:</label>
-    <input class="shadow appearance-none border rounded w-1/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" aria-label="name" name='name'>
+    <input class="form-control" type="text" aria-label="name" name='name'>
 
     <label for='price' class="block text-gray-700 text-sm font-bold mb-2">Price:</label>
-    <input class="shadow appearance-none border rounded w-1/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" aria-label="price" name='price'>
+    <input class="form-control mb-3" type="text" aria-label="price" name='price'>
 
-    <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+    <button class="btn btn-primary" type="submit">
       Add pizza
     </button>
   </div>
--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout')
+@extends('layouts.app')
 
 @section('content')
 <div class='container mx-auto p-[100px]'>
